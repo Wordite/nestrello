4b468c371f96f662210bc1052645b6ddb5ca63e3
Wordite <wordite123@gmail.com>
2024-08-24 12:05:31 +0300

    added schema, prsima module and service, use base module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookiePasrer from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookiePasrer());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUserDto';
+import { UpdateUserDto } from './dto/updateUserDto';
+
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  
+
+  @Get()
+  findAll() {
+    return this.userService.getAll()
+  }
+
+  @Get(':id')
+  find(@Param('id') id: string, @Req() req) {
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return 'user crated'
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {}
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {}
+
 }
